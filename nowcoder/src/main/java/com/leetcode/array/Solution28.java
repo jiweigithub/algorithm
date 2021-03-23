bb9a183870111d29a39a75a40d1d35cfe6d228ea
Wei Ji <wei.ji@symbio.com>
2021-03-23 18:50:17 +0800

    机试练习

---
+++ nowcoder/src/main/java/com/leetcode/array/Solution28.java
@@ -0,0 +1,79 @@
+package com.leetcode.array;
+
+/**
+ * 实现 strStr()
+ * <p>
+ * 实现 strStr() 函数。
+ * <p>
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 说明:
+ * <p>
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ */
+public class Solution28 {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("hello", "ll"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int L = needle.length();
+        int n = haystack.length();
+        for (int start = 0; start < n - L + 1; start++) {
+            if (haystack.substring(start, start + L).equals(needle)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+
+    public static int indexOf(char[] source, int sourceOffset, int sourceCount,
+                              char[] target, int targetOffset, int targetCount,
+                              int fromIndex) {
+        if (fromIndex >= sourceCount) {
+            return (targetCount == 0 ? sourceCount : -1);
+        }
+        if (fromIndex < 0) {
+            fromIndex = 0;
+        }
+        if (targetCount == 0) {
+            return fromIndex;
+        }
+
+        char first = target[targetOffset];
+        int max = sourceOffset + (sourceCount - targetCount);
+
+        for (int i = sourceOffset + fromIndex; i <= max; i++) {
+            /* Look for first character. */
+            if (source[i] != first) {
+                while (++i <= max && source[i] != first) ;
+            }
+
+            /* Found first character, now look at the rest of v2 */
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + targetCount - 1;
+                for (int k = targetOffset + 1; j < end && source[j]
+                        == target[k]; j++, k++)
+                    ;
+
+                if (j == end) {
+                    /* Found whole string. */
+                    return i - sourceOffset;
+                }
+            }
+        }
+        return -1;
+    }
+}
