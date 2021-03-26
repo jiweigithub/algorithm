ff76c3e7a273d087ec1fdfc009f897baf122f85a
Wei Ji <wei.ji@symbio.com>
2021-03-26 18:01:10 +0800

    机试练习

---
+++ nowcoder/src/main/java/com/leetcode/array/Solution67.java
@@ -0,0 +1,46 @@
+package com.leetcode.array;
+
+/**
+ * 二进制求和
+ * <p>
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * <p>
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ * <p>
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ */
+public class Solution67 {
+
+    public static void main(String[] args) {
+        Solution67 solution67 = new Solution67();
+        String a = "1";
+        String b = "111";
+        String s = solution67.addBinary(a, b);
+        System.out.println(s);
+    }
+
+
+    public String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;
+        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : "");
+        return ans.reverse().toString();
+    }
+
+}
