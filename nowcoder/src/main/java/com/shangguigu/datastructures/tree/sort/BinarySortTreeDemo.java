f72d184d6aaf8a88dfa9c4f21e19989d0ae5f761
jiwei <jiweimic@hotmail.com>
2021-04-08 23:40:03 +0800

    数据结构:二叉排序树删除节点

--- nowcoder/src/main/java/com/shangguigu/datastructures/tree/sort/BinarySortTreeDemo.java
+++ nowcoder/src/main/java/com/shangguigu/datastructures/tree/sort/BinarySortTreeDemo.java
@@ -5,15 +5,24 @@ package com.shangguigu.datastructures.tree.sort;
  */
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
         BinarySortTree binarySortTree = new BinarySortTree();
         for (int i : arr) {
             Node node = new Node(i);
             binarySortTree.add(node);
         }
+        System.out.println("删除前");
         binarySortTree.infixOrder();
-        Node node = binarySortTree.find(3);
-        System.out.println("查找到的节点：" + node);
+        binarySortTree.delete(2);
+        binarySortTree.delete(5);
+        binarySortTree.delete(9);
+        binarySortTree.delete(12);
+        binarySortTree.delete(7);
+        binarySortTree.delete(3);
+        binarySortTree.delete(10);
+        binarySortTree.delete(1);
+        System.out.println("删除后");
+        binarySortTree.infixOrder();
     }
 }
 
@@ -30,6 +39,66 @@ class BinarySortTree {
         } else {
             root.add(node);
         }
+    }
+
+    public void delete(int value) {
+        if (this.root == null) {
+            return;
+        }
+        Node targetNode = this.root.find(value);
+        if (targetNode == null) {
+            return;
+        }
+        //如果根节点是一个叶子节点，说明只有一个节点，且目标节点就是根节点
+        if (root.isLeaf()) {
+            root = null;
+            return;
+        }
+        //找到待删除节点的父节点
+        Node parent = root.findParent(value);
+        //如果targetNode是叶子节点
+        if (targetNode.isLeaf()) {
+            if (parent.right != null && targetNode.value == parent.right.value) {
+                parent.right = null;
+            } else if (parent.left != null && targetNode.value == parent.left.value) {
+                parent.left = null;
+            }
+        } else if (targetNode.hasOneLeft()) {
+            if (parent == null) {
+                root = targetNode.left;
+            } else if (parent.right != null && targetNode.value == parent.right.value) {
+                parent.right = targetNode.left;
+            } else if (parent.left != null && targetNode.value == parent.left.value) {
+                parent.left = targetNode.left;
+            }
+        } else if (targetNode.hasOneRight()) {
+            if (parent == null) {
+                root = targetNode.right;
+            } else if (parent.right != null && targetNode.value == parent.right.value) {
+                parent.right = targetNode.right;
+            } else if (parent.left != null && targetNode.value == parent.left.value) {
+                parent.left = targetNode.right;
+            }
+        } else {
+            int min = delRightTreeMin(targetNode.right);
+            targetNode.value = min;
+        }
+    }
+
+    /**
+     * 删除以node为根节点的二叉排序树的最小节点，并返回该节点的值
+     *
+     * @param node 以node为根节点的二叉排序树
+     * @return 返回以node为根节点的二叉排序树的最小节点值
+     */
+    public int delRightTreeMin(Node node) {
+        Node target = node;
+        while (target.left != null) {
+            target = target.left;
+        }
+        //此时target就指向了最小节点
+        delete(target.value);
+        return target.value;
     }
 
     public void infixOrder() {
@@ -100,10 +169,6 @@ class Node {
         }
     }
 
-    public void delete(int value) {
-
-    }
-
     public boolean isLeaf() {
         return this.left == null && this.right == null;
     }
@@ -117,7 +182,7 @@ class Node {
     }
 
     /**
-     * 查找节点
+     * 查找待删除节点
      *
      * @param value
      * @return
@@ -151,11 +216,15 @@ class Node {
         boolean rightEqual = this.right != null && this.right.value == value;
         if (leftEqual || rightEqual) {
             return this;
+        } else {
+            if (value < this.value && this.left != null) {
+                return this.left.findParent(value);
+            } else if (value >= this.value && this.right != null) {
+                return this.right.findParent(value);
+            } else {
+                return null;
+            }
         }
-        if (this.left != null && value < this.left.value) {
-            this.left.findParent(value);
-        }
-        return null;
     }
 
     /**
